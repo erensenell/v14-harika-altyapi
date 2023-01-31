b56dceeba5243736e73dc78ca6d5acfb071f6e8e
erenşenel <seneleren798@gmail.com>
2023-01-31 21:57:23 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -26,5 +26,5 @@ module.exports = async (client) => {
   }
 
     console.log(`${client.user.tag} Aktif!`);
-    client.user.setActivity("Raven Slashlı Moderasyon Botu!")
+    client.user.setActivity("Yazılım Koloniği Moderasyon Botu!")
 };
